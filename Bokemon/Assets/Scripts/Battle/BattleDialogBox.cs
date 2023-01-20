bf3f26552a4b0ad54221e88dce8b5cae1b5a2a51
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-01-20 16:09:50 +0530

    Implement the move selection and updation UI and scripts

---
+++ Bokemon/Assets/Scripts/Battle/BattleDialogBox.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour {
+    [SerializeField] Text dialogText;
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] Color highlightedColor;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    // coroutine to type out the dialog, one letter at a time
+    public IEnumerator TypeDialog(string dialog) {
+        dialogText.text = "";
+        foreach (char letter in dialog.ToCharArray()) {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+
+    // all Text based components have a enable/disable function
+    public void EnableDialogText(bool enabled) {
+        dialogText.enabled = enabled;
+    }
+
+    // ActionSelector is a GameObject, hence this property
+    public void EnableActionSelector(bool enabled) {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled) {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    // to select the action, change the color of the text
+    public void UpdateActionSelection(int selectedAction) {
+        for (int i = 0; i < actionTexts.Count; i++) {
+            if (i == selectedAction) {
+                actionTexts[i].color = highlightedColor;
+            } else {
+                actionTexts[i].color = Color.black;
+            }
+        }
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Move move) {
+        for (int i = 0; i < moveTexts.Count; i++) {
+            if (i == selectedMove) {
+                moveTexts[i].color = highlightedColor;
+            } else {
+                moveTexts[i].color = Color.black;
+            }
+        }
+
+        ppText.text = $"{move.PP}/{move.Base.PP}";
+        typeText.text = move.Base.Type.ToString();
+    }
+
+    public void SetMoveNames(List<Move> moves) {
+        for (int i = 0; i < moveTexts.Count; i++) {
+            if (i < moves.Count) {
+                moveTexts[i].text = moves[i].Base.Name;
+            } else {
+                moveTexts[i].text = "-";
+            }
+        }
+    }
+}
--- Bokemon/Assets/Scripts/Battle/BattleSystem.cs
+++ Bokemon/Assets/Scripts/Battle/BattleSystem.cs
@@ -1,6 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+// different states that define the battle
+public enum BattleState {
+    Start, 
+    PlayerAction, 
+    PlayerMove, 
+    EnemMove, 
+    Busy
+}
 
 // main battle system
 public class BattleSystem : MonoBehaviour {
@@ -8,15 +18,101 @@ public class BattleSystem : MonoBehaviour {
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    // create instance for battle state
+    BattleState state;
+
+    int currentAction;
+    int currentMove;
 
     private void Start() {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle() {
+    // the initial coroutine to display dialog and switch to player action state
+    public IEnumerator SetupBattle() {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Bokemon);
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Bokemon);
+
+        // set the moves of the player unit
+        dialogBox.SetMoveNames(playerUnit.Bokemon.Moves);
+
+        // waits for the coroutine to get completed
+        yield return dialogBox.TypeDialog("   A wild " + enemyUnit.Bokemon.Base.Name + " appeared!");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    // the series of events that happen during the player action state
+    public void PlayerAction() {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("   Choose an action."));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    // the series of events that happen during the player move state
+    public void PlayerMove() {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    private void Update() {
+        if (state == BattleState.PlayerAction) {
+            HandleActionSelector();
+        } else if (state == BattleState.PlayerMove) {
+            HandleMoveSelection();
+        }
+    }
+
+    // fight text is at index 0, run text is at index 1
+    void HandleActionSelector() {
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            if (currentAction < 1) {
+                currentAction += 1;
+            }
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (currentAction > 0) {
+                currentAction -= 1;
+            }
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z)) {
+            // fight
+            if (currentAction == 0) {
+                PlayerMove();
+            } else if (currentAction == 1) {
+                // run
+            }
+        }
+    }
+
+    void HandleMoveSelection() {
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            if (currentMove < playerUnit.Bokemon.Moves.Count - 1) {
+                currentMove += 1;
+            }
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            if (currentMove > 0) {
+                currentMove -= 1;
+            }
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            if (currentMove < playerUnit.Bokemon.Moves.Count - 2) {
+                currentMove += 2;
+            }
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            if (currentMove > 1) {
+                currentMove -= 2;
+            }
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Bokemon.Moves[currentMove]);
     }
 }
