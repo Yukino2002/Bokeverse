2dfe742e8b8c216cc81f350e4f91b1c9779deb50
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-01-22 17:46:42 +0530

    Added Basic Wallet Connect and implemented IPFS fetch

--- Bokemon/Assets/Scripts/GameController.cs
+++ Bokemon/Assets/Scripts/GameController.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using Thirdweb;
+using UnityEngine.UI;
 
 public enum GameState {
     FreeRoam,
@@ -11,16 +14,24 @@ public class GameController : MonoBehaviour {
     [SerializeField] PlayerController playerController;
     [SerializeField] BattleSystem battleSystem;
     [SerializeField] Camera worldCamera;
-    
+    public Text walletInfotext;
     GameState state;
 
+    private ThirdwebSDK sdk;
+
     private void Start() {
+        sdk = new ThirdwebSDK("goerli");
+        ConnectWallet(WalletProvider.MetaMask);
+
         // set the game state to free roam
         state = GameState.FreeRoam;
         // subscribe to the OnEncounter event
         playerController.OnEncounter += StartBattle;
         // subscribe to the OnBattleOver event
         battleSystem.OnBattleOver += EndBattle;
+
+        // // Instantise the ThirdwebSDK
+        // sdk = new ThirdwebSDK("optimism-goerli");
     }
 
     private void StartBattle() {
@@ -30,7 +41,7 @@ public class GameController : MonoBehaviour {
         battleSystem.gameObject.SetActive(true);
         // disable our main camera, so that the battle camera can be used
         worldCamera.gameObject.SetActive(false);
-
+        // await EnsureCorrectWalletState();
         // reset and start a new battle
         battleSystem.StartBattle();
     }
@@ -49,6 +60,24 @@ public class GameController : MonoBehaviour {
             playerController.HandleUpdate();
         } else if (state == GameState.Battle) {
             battleSystem.HandleUpdate();
+        }
+    }
+
+    private async void ConnectWallet(WalletProvider provider)
+    {
+        walletInfotext.text = "Connecting...";
+        try
+        {
+            string address = await sdk.wallet.Connect(new WalletConnection()
+            {
+                provider = provider,
+                chainId = 5 // Switch the wallet Goerli on connection
+            });
+            walletInfotext.text = "Connected as: " + address + " (" + provider + ")";
+        }
+        catch (System.Exception e)
+        {
+            walletInfotext.text = "Error (see console): " + e.Message;
         }
     }
 }
