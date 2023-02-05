26a396100d7ac0bcaebb9854a22b09e19f185824
Asim Jawahir <45691479+CommanderAstern@users.noreply.github.com>
2023-02-05 16:50:02 +0530

    Uploaded Game to IPFS and embedded it

---
+++ website/src/pages/game.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
+      <div className="pt-24">
+        Hi
+        <iframe src="https://gateway.ipfscdn.io/ipfs/QmXaFThYwd2ohGaD4J4SzCe47DEhQ4ZV3Kv9PLzKkVv3zG/" className = "w-full h-screen"></iframe>
+      </div>
+    );
+  }
+}
+
+export default App;
