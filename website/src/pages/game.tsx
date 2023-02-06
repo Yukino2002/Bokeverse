c2122ed79b6ecc1fb2b42aeb31499d03d654b1d5
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-02-06 19:12:25 +0530

    fix margin in the game section

--- website/src/pages/game.tsx
+++ website/src/pages/game.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div className="pt-24">
-        Hi
+      <div className="mt-[80px]">
         <iframe src="https://gateway.ipfscdn.io/ipfs/QmXaFThYwd2ohGaD4J4SzCe47DEhQ4ZV3Kv9PLzKkVv3zG/" className="w-full h-screen"></iframe>
       </div>
     );
