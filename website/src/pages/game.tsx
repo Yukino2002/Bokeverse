046feff77477de0c08cd28dba38a987cbef79ef9
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2023-02-12 15:25:43 +0530

    Update game deployment id

--- website/src/pages/game.tsx
+++ website/src/pages/game.tsx
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
         <div className="mt-[80px]">
-          <iframe src="https://gateway.ipfscdn.io/ipfs/QmXaFThYwd2ohGaD4J4SzCe47DEhQ4ZV3Kv9PLzKkVv3zG/" className="w-full h-screen"></iframe>
+          <iframe src="https://gateway.ipfscdn.io/ipfs/QmR74z73Z2SMA4jVgAkwkgyFfUk16BWLxy7uyWd23BQ2JV/" className="w-full h-screen"></iframe>
         </div>
     );
   }
